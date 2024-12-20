8dc6e15365bcc9a01d6cb0c52676577095ddae9d
uriel carcacha <urielcarcacha@gmail.com>
2024-12-20 20:42:54 -0300

    cors middleware correction

---
+++ src/middlewares/cors.middleware.js
@@ -0,0 +1,17 @@
+import ENVIROMENT from "../config/enviroment.js";
+
+const allowed_origins = [
+    'http://localhost:5173',
+    ENVIROMENT.FRONTEND_URL
+];
+
+export const customCorsMiddleware = (req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowed_origins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+};
--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ import mongoDB from "./config/mongodb.config.js"
 import statusRouter from "./routes/status.route.js"
 import productRouter from "./routes/product.route.js"
 import authRouter from "./routes/auth.route.js"
+import { customCorsMiddleware } from "./middlewares/cors.middleware.js"
 
 const PORT= 4000
 const app= express()
 
 //middlewares
+app.use(customCorsMiddleware)
 app.use(cors()) 
 app.use(express.json({limit: '3mb'}))
+
 
 app.use('/api/status', statusRouter)
 app.use('/api/auth', authRouter)
